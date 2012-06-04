105e6debc9576453077870c8aa6e061e5126d3db
Andrew Ruiz <Frost.gN@gmail.com>
2012-06-04 14:39:42 -0700

    Another GUI Update
    Adds functionality of Multiple Buttons.
    Added More Colums to store data inside of lvwList.
    Improved Functionality of viewing Ads, displaying Ad Information, and
    returning to the Ad List.
    Removed Various Labels that are no longer needed.
    Improved UI Layout.
    Removed ToolTip functionality, will likely remove completely in future
    commit.
    Added nifty highlight to title of program.

--- TimsList/TimsList/TimsList.Designer.vb
+++ TimsList/TimsList/TimsList.Designer.vb
@@ -22,6 +22,7 @@ Partial Class frmTimsList
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmTimsList))
         Me.picDoggie = New System.Windows.Forms.PictureBox()
         Me.lblTimsList = New System.Windows.Forms.Label()
@@ -29,24 +30,24 @@ Partial Class frmTimsList
         Me.lblCategorySelect = New System.Windows.Forms.Label()
         Me.txtMin = New System.Windows.Forms.TextBox()
         Me.txtMax = New System.Windows.Forms.TextBox()
-        Me.lblMin = New System.Windows.Forms.Label()
-        Me.lblMax = New System.Windows.Forms.Label()
         Me.lblPriceRange = New System.Windows.Forms.Label()
         Me.lblDash = New System.Windows.Forms.Label()
         Me.btnFindIt = New System.Windows.Forms.Button()
-        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnClearSearch = New System.Windows.Forms.Button()
         Me.pnlSearch = New System.Windows.Forms.Panel()
+        Me.lblSortBy = New System.Windows.Forms.Label()
+        Me.btnReset = New System.Windows.Forms.Button()
         Me.cmbSortOrder = New System.Windows.Forms.ComboBox()
         Me.rtbTest = New System.Windows.Forms.RichTextBox()
-        Me.lvwTest = New System.Windows.Forms.ListView()
+        Me.lvwList = New System.Windows.Forms.ListView()
         Me.pnlAd = New System.Windows.Forms.Panel()
         Me.llbBack = New System.Windows.Forms.LinkLabel()
         Me.lblAdItem = New System.Windows.Forms.Label()
         Me.picAdPicture = New System.Windows.Forms.PictureBox()
         Me.rtbAdText = New System.Windows.Forms.RichTextBox()
         Me.picComputer = New System.Windows.Forms.PictureBox()
-        Me.btnReset = New System.Windows.Forms.Button()
-        Me.lblSortBy = New System.Windows.Forms.Label()
+        Me.tipList = New System.Windows.Forms.ToolTip(Me.components)
+        Me.lblTimsListOverlay = New System.Windows.Forms.Label()
         CType(Me.picDoggie, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.pnlSearch.SuspendLayout()
         Me.pnlAd.SuspendLayout()
@@ -67,9 +68,11 @@ Partial Class frmTimsList
         'lblTimsList
         '
         Me.lblTimsList.AutoSize = True
+        Me.lblTimsList.BackColor = System.Drawing.Color.Transparent
         Me.lblTimsList.Font = New System.Drawing.Font("Impact", 27.75!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblTimsList.ForeColor = System.Drawing.Color.Black
-        Me.lblTimsList.Location = New System.Drawing.Point(63, 12)
+        Me.lblTimsList.ForeColor = System.Drawing.Color.Orange
+        Me.lblTimsList.Location = New System.Drawing.Point(63, 9)
+        Me.lblTimsList.Margin = New System.Windows.Forms.Padding(0)
         Me.lblTimsList.Name = "lblTimsList"
         Me.lblTimsList.Size = New System.Drawing.Size(158, 45)
         Me.lblTimsList.TabIndex = 1
@@ -78,10 +81,10 @@ Partial Class frmTimsList
         'cmbCategorySelect
         '
         Me.cmbCategorySelect.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
-        Me.cmbCategorySelect.Items.AddRange(New Object() {"Automobiles", "Cellular Phones", "Farm/Garden", "Furniture", "Laptop Computers", "Sporting"})
-        Me.cmbCategorySelect.Location = New System.Drawing.Point(3, 22)
+        Me.cmbCategorySelect.Items.AddRange(New Object() {"Select a Category", "Automobiles", "Cellular Phones", "Farm/Garden", "Furniture", "Laptop Computers", "Sporting"})
+        Me.cmbCategorySelect.Location = New System.Drawing.Point(58, 3)
         Me.cmbCategorySelect.Name = "cmbCategorySelect"
-        Me.cmbCategorySelect.Size = New System.Drawing.Size(205, 21)
+        Me.cmbCategorySelect.Size = New System.Drawing.Size(172, 21)
         Me.cmbCategorySelect.TabIndex = 3
         '
         'lblCategorySelect
@@ -89,13 +92,14 @@ Partial Class frmTimsList
         Me.lblCategorySelect.AutoSize = True
         Me.lblCategorySelect.Location = New System.Drawing.Point(3, 6)
         Me.lblCategorySelect.Name = "lblCategorySelect"
-        Me.lblCategorySelect.Size = New System.Drawing.Size(85, 13)
+        Me.lblCategorySelect.Size = New System.Drawing.Size(52, 13)
         Me.lblCategorySelect.TabIndex = 4
-        Me.lblCategorySelect.Text = "Select Category:"
+        Me.lblCategorySelect.Text = "Category:"
         '
         'txtMin
         '
-        Me.txtMin.Location = New System.Drawing.Point(301, 22)
+        Me.txtMin.ForeColor = System.Drawing.Color.DarkGray
+        Me.txtMin.Location = New System.Drawing.Point(326, 6)
         Me.txtMin.Name = "txtMin"
         Me.txtMin.Size = New System.Drawing.Size(72, 20)
         Me.txtMin.TabIndex = 5
@@ -103,34 +107,17 @@ Partial Class frmTimsList
         '
         'txtMax
         '
-        Me.txtMax.Location = New System.Drawing.Point(408, 22)
+        Me.txtMax.ForeColor = System.Drawing.Color.DarkGray
+        Me.txtMax.Location = New System.Drawing.Point(433, 6)
         Me.txtMax.Name = "txtMax"
         Me.txtMax.Size = New System.Drawing.Size(71, 20)
         Me.txtMax.TabIndex = 6
         Me.txtMax.Text = "Max"
         '
-        'lblMin
-        '
-        Me.lblMin.AutoSize = True
-        Me.lblMin.Location = New System.Drawing.Point(313, 6)
-        Me.lblMin.Name = "lblMin"
-        Me.lblMin.Size = New System.Drawing.Size(48, 13)
-        Me.lblMin.TabIndex = 7
-        Me.lblMin.Text = "Minimum"
-        '
-        'lblMax
-        '
-        Me.lblMax.AutoSize = True
-        Me.lblMax.Location = New System.Drawing.Point(417, 5)
-        Me.lblMax.Name = "lblMax"
-        Me.lblMax.Size = New System.Drawing.Size(51, 13)
-        Me.lblMax.TabIndex = 8
-        Me.lblMax.Text = "Maximum"
-        '
         'lblPriceRange
         '
         Me.lblPriceRange.AutoSize = True
-        Me.lblPriceRange.Location = New System.Drawing.Point(214, 25)
+        Me.lblPriceRange.Location = New System.Drawing.Point(239, 9)
         Me.lblPriceRange.Name = "lblPriceRange"
         Me.lblPriceRange.Size = New System.Drawing.Size(81, 13)
         Me.lblPriceRange.TabIndex = 9
@@ -139,7 +126,7 @@ Partial Class frmTimsList
         'lblDash
         '
         Me.lblDash.AutoSize = True
-        Me.lblDash.Location = New System.Drawing.Point(379, 25)
+        Me.lblDash.Location = New System.Drawing.Point(404, 9)
         Me.lblDash.Name = "lblDash"
         Me.lblDash.Size = New System.Drawing.Size(25, 13)
         Me.lblDash.TabIndex = 10
@@ -147,21 +134,21 @@ Partial Class frmTimsList
         '
         'btnFindIt
         '
-        Me.btnFindIt.Location = New System.Drawing.Point(493, 20)
+        Me.btnFindIt.Location = New System.Drawing.Point(518, 4)
         Me.btnFindIt.Name = "btnFindIt"
         Me.btnFindIt.Size = New System.Drawing.Size(75, 23)
         Me.btnFindIt.TabIndex = 12
         Me.btnFindIt.Text = "&Find It"
         Me.btnFindIt.UseVisualStyleBackColor = True
         '
-        'btnClear
+        'btnClearSearch
         '
-        Me.btnClear.Location = New System.Drawing.Point(574, 20)
-        Me.btnClear.Name = "btnClear"
-        Me.btnClear.Size = New System.Drawing.Size(75, 23)
-        Me.btnClear.TabIndex = 13
-        Me.btnClear.Text = "&Clear Entires"
-        Me.btnClear.UseVisualStyleBackColor = True
+        Me.btnClearSearch.Location = New System.Drawing.Point(599, 4)
+        Me.btnClearSearch.Name = "btnClearSearch"
+        Me.btnClearSearch.Size = New System.Drawing.Size(75, 23)
+        Me.btnClearSearch.TabIndex = 13
+        Me.btnClearSearch.Text = "&Clear Entires"
+        Me.btnClearSearch.UseVisualStyleBackColor = True
         '
         'pnlSearch
         '
@@ -171,28 +158,44 @@ Partial Class frmTimsList
         Me.pnlSearch.Controls.Add(Me.btnReset)
         Me.pnlSearch.Controls.Add(Me.cmbSortOrder)
         Me.pnlSearch.Controls.Add(Me.cmbCategorySelect)
-        Me.pnlSearch.Controls.Add(Me.btnClear)
+        Me.pnlSearch.Controls.Add(Me.btnClearSearch)
         Me.pnlSearch.Controls.Add(Me.lblCategorySelect)
         Me.pnlSearch.Controls.Add(Me.btnFindIt)
         Me.pnlSearch.Controls.Add(Me.txtMin)
         Me.pnlSearch.Controls.Add(Me.txtMax)
         Me.pnlSearch.Controls.Add(Me.lblDash)
-        Me.pnlSearch.Controls.Add(Me.lblMin)
         Me.pnlSearch.Controls.Add(Me.lblPriceRange)
-        Me.pnlSearch.Controls.Add(Me.lblMax)
         Me.pnlSearch.Location = New System.Drawing.Point(12, 64)
         Me.pnlSearch.Name = "pnlSearch"
         Me.pnlSearch.Size = New System.Drawing.Size(994, 59)
         Me.pnlSearch.TabIndex = 14
         '
+        'lblSortBy
+        '
+        Me.lblSortBy.AutoSize = True
+        Me.lblSortBy.Location = New System.Drawing.Point(689, 9)
+        Me.lblSortBy.Name = "lblSortBy"
+        Me.lblSortBy.Size = New System.Drawing.Size(44, 13)
+        Me.lblSortBy.TabIndex = 16
+        Me.lblSortBy.Text = "Sort By:"
+        '
+        'btnReset
+        '
+        Me.btnReset.Location = New System.Drawing.Point(896, 4)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(84, 23)
+        Me.btnReset.TabIndex = 15
+        Me.btnReset.Text = "Reset Sorting"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
         'cmbSortOrder
         '
         Me.cmbSortOrder.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.cmbSortOrder.FormattingEnabled = True
-        Me.cmbSortOrder.Items.AddRange(New Object() {"Item Name (A-Z)", "Item Name (Z-A)", "Category (A-Z)", "Category (Z-A)", "Price (Low to High)", "Price (High to Low)"})
-        Me.cmbSortOrder.Location = New System.Drawing.Point(702, 22)
+        Me.cmbSortOrder.Items.AddRange(New Object() {"Choose a Sorting Method", "Item Name (A-Z)", "Item Name (Z-A)", "Category (A-Z)", "Category (Z-A)", "Price (Low to High)", "Price (High to Low)"})
+        Me.cmbSortOrder.Location = New System.Drawing.Point(739, 3)
         Me.cmbSortOrder.Name = "cmbSortOrder"
-        Me.cmbSortOrder.Size = New System.Drawing.Size(188, 21)
+        Me.cmbSortOrder.Size = New System.Drawing.Size(151, 21)
         Me.cmbSortOrder.TabIndex = 14
         '
         'rtbTest
@@ -206,20 +209,24 @@ Partial Class frmTimsList
         Me.rtbTest.Text = "Used to output different debugging information. Will not be included in final rel" & _
             "ease. Or it could be turned into a scrolling marquee of our names :P"
         '
-        'lvwTest
+        'lvwList
         '
-        Me.lvwTest.BackColor = System.Drawing.Color.Bisque
-        Me.lvwTest.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lvwTest.FullRowSelect = True
-        Me.lvwTest.GridLines = True
-        Me.lvwTest.LabelWrap = False
-        Me.lvwTest.Location = New System.Drawing.Point(12, 129)
-        Me.lvwTest.MultiSelect = False
-        Me.lvwTest.Name = "lvwTest"
-        Me.lvwTest.Size = New System.Drawing.Size(994, 578)
-        Me.lvwTest.TabIndex = 21
-        Me.lvwTest.UseCompatibleStateImageBehavior = False
-        Me.lvwTest.View = System.Windows.Forms.View.Details
+        Me.lvwList.Activation = System.Windows.Forms.ItemActivation.OneClick
+        Me.lvwList.BackColor = System.Drawing.Color.Bisque
+        Me.lvwList.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lvwList.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lvwList.FullRowSelect = True
+        Me.lvwList.HideSelection = False
+        Me.lvwList.HoverSelection = True
+        Me.lvwList.LabelWrap = False
+        Me.lvwList.Location = New System.Drawing.Point(12, 129)
+        Me.lvwList.MultiSelect = False
+        Me.lvwList.Name = "lvwList"
+        Me.lvwList.Scrollable = False
+        Me.lvwList.Size = New System.Drawing.Size(994, 535)
+        Me.lvwList.TabIndex = 21
+        Me.lvwList.UseCompatibleStateImageBehavior = False
+        Me.lvwList.View = System.Windows.Forms.View.Details
         '
         'pnlAd
         '
@@ -228,26 +235,27 @@ Partial Class frmTimsList
         Me.pnlAd.Controls.Add(Me.lblAdItem)
         Me.pnlAd.Controls.Add(Me.picAdPicture)
         Me.pnlAd.Controls.Add(Me.rtbAdText)
-        Me.pnlAd.Location = New System.Drawing.Point(110, 186)
+        Me.pnlAd.Location = New System.Drawing.Point(126, 225)
         Me.pnlAd.Name = "pnlAd"
-        Me.pnlAd.Size = New System.Drawing.Size(836, 508)
+        Me.pnlAd.Size = New System.Drawing.Size(867, 425)
         Me.pnlAd.TabIndex = 26
+        Me.pnlAd.Visible = False
         '
         'llbBack
         '
         Me.llbBack.AutoSize = True
-        Me.llbBack.Location = New System.Drawing.Point(761, 8)
+        Me.llbBack.Location = New System.Drawing.Point(735, 15)
         Me.llbBack.Name = "llbBack"
-        Me.llbBack.Size = New System.Drawing.Size(67, 13)
+        Me.llbBack.Size = New System.Drawing.Size(101, 13)
         Me.llbBack.TabIndex = 3
         Me.llbBack.TabStop = True
-        Me.llbBack.Text = "<<  Go Back"
+        Me.llbBack.Text = "<<-  Go Back to List"
         '
         'lblAdItem
         '
         Me.lblAdItem.AutoSize = True
         Me.lblAdItem.Font = New System.Drawing.Font("Arial Black", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblAdItem.Location = New System.Drawing.Point(34, 0)
+        Me.lblAdItem.Location = New System.Drawing.Point(3, 0)
         Me.lblAdItem.Name = "lblAdItem"
         Me.lblAdItem.Size = New System.Drawing.Size(297, 45)
         Me.lblAdItem.TabIndex = 2
@@ -255,18 +263,18 @@ Partial Class frmTimsList
         '
         'picAdPicture
         '
-        Me.picAdPicture.Location = New System.Drawing.Point(30, 48)
+        Me.picAdPicture.Location = New System.Drawing.Point(11, 48)
         Me.picAdPicture.Name = "picAdPicture"
-        Me.picAdPicture.Size = New System.Drawing.Size(351, 331)
+        Me.picAdPicture.Size = New System.Drawing.Size(442, 333)
         Me.picAdPicture.TabIndex = 1
         Me.picAdPicture.TabStop = False
         '
         'rtbAdText
         '
         Me.rtbAdText.BackColor = System.Drawing.SystemColors.Window
-        Me.rtbAdText.Location = New System.Drawing.Point(440, 48)
+        Me.rtbAdText.Location = New System.Drawing.Point(459, 48)
         Me.rtbAdText.Name = "rtbAdText"
-        Me.rtbAdText.Size = New System.Drawing.Size(377, 425)
+        Me.rtbAdText.Size = New System.Drawing.Size(377, 333)
         Me.rtbAdText.TabIndex = 0
         Me.rtbAdText.Text = ""
         '
@@ -279,37 +287,45 @@ Partial Class frmTimsList
         Me.picComputer.TabIndex = 27
         Me.picComputer.TabStop = False
         '
-        'btnReset
+        'tipList
         '
-        Me.btnReset.Location = New System.Drawing.Point(896, 20)
-        Me.btnReset.Name = "btnReset"
-        Me.btnReset.Size = New System.Drawing.Size(84, 23)
-        Me.btnReset.TabIndex = 15
-        Me.btnReset.Text = "Reset Sorting"
-        Me.btnReset.UseVisualStyleBackColor = True
+        Me.tipList.AutoPopDelay = 5000
+        Me.tipList.InitialDelay = 250
+        Me.tipList.IsBalloon = True
+        Me.tipList.ReshowDelay = 100
+        Me.tipList.ToolTipTitle = "ACK"
+        Me.tipList.UseAnimation = False
+        Me.tipList.UseFading = False
         '
-        'lblSortBy
+        'lblTimsListOverlay
         '
-        Me.lblSortBy.AutoSize = True
-        Me.lblSortBy.Location = New System.Drawing.Point(699, 6)
-        Me.lblSortBy.Name = "lblSortBy"
-        Me.lblSortBy.Size = New System.Drawing.Size(44, 13)
-        Me.lblSortBy.TabIndex = 16
-        Me.lblSortBy.Text = "Sort By:"
+        Me.lblTimsListOverlay.AutoSize = True
+        Me.lblTimsListOverlay.BackColor = System.Drawing.Color.Transparent
+        Me.lblTimsListOverlay.Font = New System.Drawing.Font("Impact", 27.75!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTimsListOverlay.ForeColor = System.Drawing.Color.Black
+        Me.lblTimsListOverlay.Location = New System.Drawing.Point(277, 9)
+        Me.lblTimsListOverlay.Margin = New System.Windows.Forms.Padding(0)
+        Me.lblTimsListOverlay.Name = "lblTimsListOverlay"
+        Me.lblTimsListOverlay.Size = New System.Drawing.Size(158, 45)
+        Me.lblTimsListOverlay.TabIndex = 28
+        Me.lblTimsListOverlay.Text = "Tim's List"
         '
         'frmTimsList
         '
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
-        Me.ClientSize = New System.Drawing.Size(1018, 706)
+        Me.ClientSize = New System.Drawing.Size(1018, 676)
         Me.Controls.Add(Me.picComputer)
         Me.Controls.Add(Me.pnlAd)
         Me.Controls.Add(Me.rtbTest)
-        Me.Controls.Add(Me.lvwTest)
+        Me.Controls.Add(Me.lvwList)
         Me.Controls.Add(Me.pnlSearch)
         Me.Controls.Add(Me.lblTimsList)
         Me.Controls.Add(Me.picDoggie)
+        Me.Controls.Add(Me.lblTimsListOverlay)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.MaximizeBox = False
         Me.Name = "frmTimsList"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Our Tim's List Project"
         CType(Me.picDoggie, System.ComponentModel.ISupportInitialize).EndInit()
         Me.pnlSearch.ResumeLayout(False)
@@ -328,15 +344,13 @@ Partial Class frmTimsList
     Friend WithEvents lblCategorySelect As System.Windows.Forms.Label
     Friend WithEvents txtMin As System.Windows.Forms.TextBox
     Friend WithEvents txtMax As System.Windows.Forms.TextBox
-    Friend WithEvents lblMin As System.Windows.Forms.Label
-    Friend WithEvents lblMax As System.Windows.Forms.Label
     Friend WithEvents lblPriceRange As System.Windows.Forms.Label
     Friend WithEvents lblDash As System.Windows.Forms.Label
     Friend WithEvents btnFindIt As System.Windows.Forms.Button
-    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnClearSearch As System.Windows.Forms.Button
     Friend WithEvents pnlSearch As System.Windows.Forms.Panel
     Friend WithEvents rtbTest As System.Windows.Forms.RichTextBox
-    Friend WithEvents lvwTest As System.Windows.Forms.ListView
+    Friend WithEvents lvwList As System.Windows.Forms.ListView
     Friend WithEvents pnlAd As System.Windows.Forms.Panel
     Friend WithEvents lblAdItem As System.Windows.Forms.Label
     Friend WithEvents picAdPicture As System.Windows.Forms.PictureBox
@@ -346,5 +360,7 @@ Partial Class frmTimsList
     Friend WithEvents picComputer As System.Windows.Forms.PictureBox
     Friend WithEvents btnReset As System.Windows.Forms.Button
     Friend WithEvents lblSortBy As System.Windows.Forms.Label
+    Friend WithEvents tipList As System.Windows.Forms.ToolTip
+    Friend WithEvents lblTimsListOverlay As System.Windows.Forms.Label
 
 End Class
--- TimsList/TimsList/TimsList.vb
+++ TimsList/TimsList/TimsList.vb
@@ -28,38 +28,168 @@ Public Class frmTimsList
 
     Private Sub TimsList_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
+        Const shtOUTER_MARGIN As Short = 10
+        Const shtINNER_MARGIN As Short = 5
+        Const shtBORDER As Short = 2
+
+        ' Description:
+        '  Sets locations of objects definitively
+
+        picDoggie.Left = shtOUTER_MARGIN
+        picDoggie.Top = shtOUTER_MARGIN
+
+        lblTimsList.Left = picDoggie.Left + picDoggie.Width
+        lblTimsList.Top = shtOUTER_MARGIN
+
+        picComputer.Left = lblTimsList.Left + lblTimsList.Width
+        picComputer.Top = shtOUTER_MARGIN
+        'May Need to add Marquee information here...
+
+        'Creates a neat "highlighting" effect of the title
+        lblTimsListOverlay.Parent = lblTimsList
+        lblTimsListOverlay.Top = 1
+        lblTimsListOverlay.Left = 1
+
+        'Sets individual locations of objects inside of pnlSearch
+        cmbCategorySelect.Top = shtINNER_MARGIN
+        'Causes the combo box to default to the text, "Select a Category!"
+        cmbCategorySelect.SelectedIndex = 0
+
+        'Sets pnlSearch size according to inner objects, also location of pnlSearch.
+        pnlSearch.Top = shtOUTER_MARGIN + picDoggie.Height + shtINNER_MARGIN
+        pnlSearch.Left = shtOUTER_MARGIN
+        pnlSearch.Width = Me.ClientSize.Width - shtOUTER_MARGIN * 2
+        pnlSearch.Height = cmbCategorySelect.Height + shtINNER_MARGIN * 2 + shtBORDER
+
+        'Sets lvwList location
+        lvwList.Top = pnlSearch.Top + pnlSearch.Height + shtINNER_MARGIN
+        lvwList.Left = shtOUTER_MARGIN
+        lvwList.Width = Me.ClientSize.Width - shtOUTER_MARGIN * 2
+        lvwList.Height = Me.ClientSize.Height - shtOUTER_MARGIN * 2 - shtINNER_MARGIN * 2 - pnlSearch.Height - picDoggie.Height
+
+
+
+
         'Test Setup for List View Columns.
         ' ...Add(Description as String, Width in Pixels, Alignment)
-        lvwTest.Columns.Add("Item Name", 200, HorizontalAlignment.Left)
-        lvwTest.Columns.Add("Category", 150, HorizontalAlignment.Center)
-        lvwTest.Columns.Add("Short Description", 500, HorizontalAlignment.Left)
-        lvwTest.Columns.Add("Price", 90, HorizontalAlignment.Left)
+        lvwList.Columns.Add("Item Name", 220, HorizontalAlignment.Left)
+        lvwList.Columns.Add("Category", 170, HorizontalAlignment.Center)
+        lvwList.Columns.Add("Short Description", 500, HorizontalAlignment.Left)
+        lvwList.Columns.Add("Price", 100, HorizontalAlignment.Left)
+        lvwList.Columns.Add("Long Description", 0)
+        lvwList.Columns.Add("Picture Filename", 0)
 
 
         Dim item1 As New ListViewItem("Alienware MX11", 0)
         item1.SubItems.Add("Laptop (Computers)")
         item1.SubItems.Add("High-end gaming computer at a discount.")
         item1.SubItems.Add(Chr(36) & "775.00")
+        item1.SubItems.Add("ASDFASDFASDFASDFASDFASDFSAf")
+        item1.SubItems.Add("lienware.jpg")
 
-        lvwTest.Items.Add(item1)
+        lvwList.Items.Add(item1)
 
         Dim item2 As New ListViewItem("Ford Model T", 1)
         item2.SubItems.Add("Automobiles")
         item2.SubItems.Add("The original mass-produced motor vehicle!")
         item2.SubItems.Add(Chr(36) & "20000.00")
+        item2.SubItems.Add("a46246856t5A#EG")
+        item2.SubItems.Add("modelt.jpg")
 
-        lvwTest.Items.Add(item2)
+        lvwList.Items.Add(item2)
 
         'Test Data for pnlAd
 
         lblAdItem.Text = "Alienware MX11"
         picAdPicture.Image = Image.FromFile("../../My Project/DataFiles/lienware.jpg")
         picAdPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
-                picComputer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
-        'FileOpen(1, "../../My Project/DataFiles/lienware.tlf", OpenMode.Input)
+        picComputer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         rtbAdText.Text = File.ReadAllText("../../My Project/DataFiles/lienware.tlf")
-        'FileClose(1)
 
+    End Sub
+
+
+    Private Sub lvwList_MouseClick(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles lvwList.MouseClick
+        'Test output for selecting an item and updating pnlAd objects with correct information.
+        lblAdItem.Text = lvwList.SelectedItems.Item(0).Text
+        rtbAdText.Text = "Category:" & lvwList.SelectedItems.Item(0).SubItems(1).Text & Chr(13) & Chr(13) & _
+            "'Long' Description:" & Chr(13) & lvwList.SelectedItems.Item(0).SubItems(4).Text & Chr(13) & Chr(13) & _
+            "Price: " & lvwList.SelectedItems.Item(0).SubItems(3).Text
+        picAdPicture.Image = Image.FromFile("../../My Project/DataFiles/" & lvwList.SelectedItems.Item(0).SubItems(5).Text)
+        pnlAd.Visible = True
+    End Sub
+
+    'Private Sub lvwList_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles lvwList.MouseMove
+    '   'Ensures the tooltip is removed if no item is currently being hovered over.
+    '    If lvwList.GetItemAt(e.X, e.Y) Is Nothing Then
+    '        tipList.RemoveAll()
+    '    End If
+    'End Sub
+
+    Private Sub txtMin_GotFocus(ByVal sender As Object, ByVal e As System.EventArgs) Handles txtMin.GotFocus
+        'Clears txtMin when the user "first" selects into the box
+        If txtMin.Text = "Min" Then
+            txtMin.Text = String.Empty
+            txtMin.ForeColor = Color.Black
+        End If
+    End Sub
+
+    Private Sub txtMax_GotFocus(ByVal sender As Object, ByVal e As System.EventArgs) Handles txtMax.GotFocus
+        'Clears txtMax when the user "first" clicks into the box
+        If txtMax.Text = "Max" Then
+            txtMax.Text = String.Empty
+            txtMax.ForeColor = Color.Black
+        End If
+
+    End Sub
+
+    Private Sub txtMin_LostFocus(ByVal sender As Object, ByVal e As System.EventArgs) Handles txtMin.LostFocus
+        'Sets txtMin to defaults if user clears the field or enters nothing.
+
+        If txtMin.Text = String.Empty Then
+            txtMin.ForeColor = Color.DarkGray
+            txtMin.Text = "Min"
+        End If
+
+    End Sub
+
+    Private Sub txtMax_LostFocus(ByVal sender As Object, ByVal e As System.EventArgs) Handles txtMax.LostFocus
+        'Sets txtMax to default if user clears field or enters nothing.
+
+        If txtMax.Text = String.Empty Then
+            txtMax.ForeColor = Color.DarkGray
+            txtMax.Text = "Max"
+        End If
+
+    End Sub
+
+    Private Sub llbBack_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles llbBack.Click
+        'Hides pnlAd when user is finished viewing add and returns focus to lvwList
+
+        pnlAd.Visible = False
+        lvwList.Focus()
+
+    End Sub
+
+    Private Sub btnFindIt_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnFindIt.Click
+        'Will eventually sort through data files and display to lvwList appropriate information.
+        'Also, hides pnlAd in case if user changes their mind about the type of product they're interested in.
+        'Currently outputs to rtbTest as String data.
+
+        rtbTest.Text = "Searched>>>" & Chr(13) & "Category: " & cmbCategorySelect.SelectedItem.ToString & Chr(9) & "Min Price: " & txtMin.Text & Chr(9) & "Max Price: " & txtMax.Text
+        pnlAd.Visible = False
+
+    End Sub
+
+    Private Sub btnClearSearch_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnClearSearch.Click
+        'Sets the search values back to their defaults values. Does not affect sorting.
+        'Calls txtMin_LostFocus and txtMax_LostFocus
+
+        cmbCategorySelect.SelectedIndex = 0
+        txtMin.Text = String.Empty
+        Call txtMin_LostFocus(sender, e)
+        txtMax.Text = String.Empty
+        Call txtMax_LostFocus(sender, e)
     End Sub
 
 End Class
